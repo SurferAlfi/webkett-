3ffbb05f9be22911ea702074ff900773666de251
gyel123 <vizes35@gmail.com>
2023-11-19 19:18:52 +0100

    user.php, welcome.php módosítás

--- db_connection.php
+++ db_connection.php
@@ -10,4 +10,21 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Sikertelen kapcsolódás az adatbázishoz: " . $conn->connect_error);
 }
+// db_connection.php
+class Database {
+    private $conn;
+
+    public function __construct($servername, $username, $password, $dbname) {
+        $this->conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($this->conn->connect_error) {
+            die("Sikertelen kapcsolódás az adatbázishoz: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+
 ?>
